1df8cb1f554c13f28073173a16343f8a5a5bfaf7
Antoine <antoine.pommert@gmail.com>
2021-07-05 14:24:37 +0100

    language page new layout

--- greek/_data/languageData.js
+++ greek/_data/languageData.js
@@ -5,7 +5,7 @@ const language = "el-GR";
 const activateSpeech = true;
 
 const languageContent = [
-    { "title": "Alphabet (lowercase)", "icon": "α", "url": "alphabet-lower-case", "localStorageKey": "greek" + "LowerCaseCharacters" + "Srs" },
-    { "title": "Alphabet (uppercase)", "icon": "Α", "url": "alphabet-upper-case", "localStorageKey": "greek" + "UpperCaseCharacters" + "Srs" },
-    { "title": "Alphabet (all case)", "icon": "αΑ", "url": "alphabet-all-characters", "localStorageKey": "greek" + "AllCaseCharacters" + "Srs" },
+    { "title": "Alphabet (lowercase)", "sub": "Practice the lowercase letters from the Greek alphabet", "icon": "α", "url": "alphabet-lower-case", "localStorageKey": "greek" + "LowerCaseCharacters" + "Srs" },
+    { "title": "Alphabet (uppercase)", "sub": "Practice the uppercase letters from the Greek alphabet", "icon": "Α", "url": "alphabet-upper-case", "localStorageKey": "greek" + "UpperCaseCharacters" + "Srs" },
+    { "title": "Alphabet (all case)", "sub": "Practice all letters from the Greek alphabet", "icon": "αΑ", "url": "alphabet-all-characters", "localStorageKey": "greek" + "AllCaseCharacters" + "Srs" },
 ]
--- russian/_data/languageData.js
+++ russian/_data/languageData.js
@@ -6,6 +6,6 @@ const activateSpeech = true;
 
 const languageContent = [
     { "title": "Alphabet (lowercase)", "icon": "a", "url": "alphabet-lower-case", "localStorageKey": "russian" + "LowerCaseCharacters" + "Srs" },
-    { "title": "Alphabet (uppercase)", "icon": "A", "url": "alphabet-upper-case", "localStorageKey": "russian" + "UpperCaseCharacters" + "Srs" },
+    { "title": "Alphabet (uppercase", "icon": "A", "url": "alphabet-upper-case", "localStorageKey": "russian" + "UpperCaseCharacters" + "Srs" },
     { "title": "Alphabet (all case)", "icon": "aA", "url": "alphabet-all-characters", "localStorageKey": "russian" + "AllCaseCharacters" + "Srs" },
 ]
