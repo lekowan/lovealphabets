76645c7b93c9970a3609c710797f9998932215d4
Antoine <antoine.pommert@gmail.com>
2021-07-05 17:34:38 +0100

    update terminology

--- amharic/_data/languageData.js
+++ amharic/_data/languageData.js
@@ -3,5 +3,5 @@ const languageSub = "አማርኛ";
 
 
 const languageContent = [
-    { "title": "All characters",  "sub": "Practice all characters from the Amharic alphabet", "view": "View the characters", "info": "286 fidels", "icon": "አ", "url": "all-characters", "localStorageKey": "amharic" + "AllCharacters" + "Srs" },
+    { "title": "Fidäl",  "sub": "Practice all fidels (characters) from the Amharic script", "view": "View the characters", "info": "286 fidels", "icon": "አ", "url": "all-characters", "localStorageKey": "amharic" + "AllCharacters" + "Srs" },
 ]
--- inuktitut/_data/languageData.js
+++ inuktitut/_data/languageData.js
@@ -3,5 +3,5 @@ const languageSub = "ᖃᓂᐅᔮᖅᐸᐃᑦ";
 const languageGeo = "Nunavik";
 
 const languageContent = [
-    { "title": "All characters", "sub": "Practice all characters from the Inuktitut syllabics", "view": "View the syllabics", "info": "111 syllables", "icon": "ᐃ", "url": "all-characters", "localStorageKey": "inuktitut" + "Characters" + "Srs" },
+    { "title": "Syllabics", "sub": "Practice all syllables from the Inuktitut syllabics", "view": "View the syllabics", "info": "111 syllables", "icon": "ᐃ", "url": "all-characters", "localStorageKey": "inuktitut" + "Characters" + "Srs" },
 ]
