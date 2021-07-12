828aa05af1408ef824fec2d5f35b75934e179097
Sean Turley <seanmturley@gmail.com>
2021-07-12 18:13:14 +0200

    Simplify shuffle function code

--- _global/js/srsExerciseModel.js
+++ _global/js/srsExerciseModel.js
@@ -221,22 +221,10 @@ class Model {
 
   // Shuffle Array Function
   shuffle(array) {
-    let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+    for (let i = array.length - 1; i > 0; i--) {
+      const randIndex = Math.floor(Math.random() * (i + 1));
+      [array[i], array[randIndex]] = [array[randIndex], array[i]];
     }
-
     return array;
   }
 
