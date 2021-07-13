27f0c020869a5b1bb20a1a0773636f42c9831de1
Antoine <antoine.pommert@gmail.com>
2021-07-13 20:14:39 +0100

    update

---
+++ inuktitut/i-column/_data/inuktitutMap.js
@@ -0,0 +1,21 @@
+const inuktitutIColumnMap = {
+ 
+  "ᐃ": { "letter" :  "i" },
+  "ᐱ": { "letter" :  "pi" },
+  "ᑎ": { "letter" :  "ti" },
+  "ᑭ": { "letter" :  "ki" },
+  "ᒋ": { "letter" :  "gi" },
+  "ᒥ": { "letter" :  "mi" },
+  "ᓂ": { "letter" :  "ni" },
+  "ᓯ": { "letter" :  "si" },
+  "ᓕ": { "letter" :  "li" },
+  "ᔨ": { "letter" :  "ji" },
+  "ᕕ": { "letter" :  "vi" },
+  "ᕆ": { "letter" :  "ri" },
+  "ᕿ": { "letter" :  "qi" },
+  "ᖏ": { "letter" :  "ngi" },
+}
+
+const inuktitutIColumnChartData = [
+  { "subtitle": "", "content": inuktitutIColumnMap }, 
+]
