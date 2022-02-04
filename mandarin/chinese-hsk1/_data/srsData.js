fdb6dc53402d5ddc6e13589791e1b59116d15d72
lekowan <antoine.pommert@gmail.com>
2022-02-04 19:15:46 +0000

    data Test

--- mandarin/chinese-hsk1/_data/srsData.js
+++ mandarin/chinese-hsk1/_data/srsData.js
@@ -23,7 +23,7 @@ const welcomeIntro = "In this spaced-repetition practice (SRS), you will study n
 const allSyllableMap = {
     "1": {
         "character": "我",
-        "letter": "I or me",
+        "letter": "I or I",
         "pinyin": "wǒ"
     },
     "2": {
