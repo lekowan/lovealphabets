80058d0da543a2bfec5fea4ca4caa280f2c77785
lekowan <antoine.pommert@gmail.com>
2022-03-01 22:02:58 +0000

    create JP Shirokuma ep20

---
+++ japanese/_data/languageData.js
@@ -0,0 +1,18 @@
+const languageTitle = "Japanese";
+const languageSub = "日本語";
+
+// Speech Synthesis
+const language = "ja-JP";
+const activateSpeech = true;
+
+const languageContent = [{
+      "chartTitle": "Shirokuma / Episode 20",
+      "chartName": "shirokuma-episode-20-map",
+      "chartData": shirokumaEpisode20ChartData,
+      "title": "Shirokuma Episode 20",
+      "sub": "Memorize all words from Shirokuma Episode 20",
+      "icon": "ペ",
+      "url": "shirokuma-episode-20",
+      "localStorageKey": "japanese" + "Episode20" + "Srs",
+      "chartType": shirokumaEpisode20ChartType
+}, ]
