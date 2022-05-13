4bd099c085b334c1610eef3c7f1c59b5a4b91a08
lekowan <antoine.pommert@gmail.com>
2022-05-13 12:40:54 +0100

    created Russian alphabet SRS

--- _languages_to_be_updated/russian/alphabet-all-characters/_data/russianAllCharactersMap.js
+++
@@ -1,4 +0,0 @@
-const russianChartData = [
-    { "subtitle": "Uppercase letters", "content": russianUpperCaseMap }, 
-    { "subtitle": "Lowercase letters", "content": russianLowerCaseMap  }, 
-]
---
+++ russian/alphabet-all-characters/_data/russianAllCharactersMap.js
@@ -0,0 +1,335 @@
+const allRussianCharacterMap = {
+    "1": {
+        "character": "А",
+        "letter": "a"
+    },
+    "2": {
+        "character": "Б",
+        "letter": "b"
+    },
+    "3": {
+        "character": "В",
+        "letter": "v"
+    },
+    "4": {
+        "character": "Г",
+        "letter": "g"
+    },
+    "5": {
+        "character": "Д",
+        "letter": "d"
+    },
+    "6": {
+        "character": "Е",
+        "letter": [
+            "ye",
+            "je",
+            "e"
+        ]
+    },
+    "7": {
+        "character": "Ё",
+        "letter": [
+            "yo",
+            "jo"
+        ]
+    },
+    "8": {
+        "character": "Ж",
+        "letter": "zh"
+    },
+    "9": {
+        "character": "З",
+        "letter": "z"
+    },
+    "10": {
+        "character": "И",
+        "letter": "i"
+    },
+    "11": {
+        "character": "Й",
+        "letter": [
+            "y",
+            "i",
+            "j"
+        ]
+    },
+    "12": {
+        "character": "К",
+        "letter": "k"
+    },
+    "13": {
+        "character": "Л",
+        "letter": "l"
+    },
+    "14": {
+        "character": "М",
+        "letter": "m"
+    },
+    "15": {
+        "character": "Н",
+        "letter": "n"
+    },
+    "16": {
+        "character": "О",
+        "letter": "o"
+    },
+    "17": {
+        "character": "П",
+        "letter": "p"
+    },
+    "18": {
+        "character": "Р",
+        "letter": "r"
+    },
+    "19": {
+        "character": "С",
+        "letter": "s"
+    },
+    "20": {
+        "character": "Т",
+        "letter": "t"
+    },
+    "21": {
+        "character": "У",
+        "letter": "u"
+    },
+    "22": {
+        "character": "Ф",
+        "letter": "f"
+    },
+    "23": {
+        "character": "Х",
+        "letter": [
+            "kh",
+            "h"
+        ]
+    },
+    "24": {
+        "character": "Ц",
+        "letter": [
+            "ts",
+            "c"
+        ]
+    },
+    "25": {
+        "character": "Ч",
+        "letter": "ch"
+    },
+    "26": {
+        "character": "Ш",
+        "letter": [
+            "sh",
+            "s"
+        ]
+    },
+    "27": {
+        "character": "Щ",
+        "letter": [
+            "shch",
+            "sch"
+        ]
+    },
+    "28": {
+        "character": "Ъ",
+        "letter": "-"
+    },
+    "29": {
+        "character": "Ы",
+        "letter": "y"
+    },
+    "30": {
+        "character": "Ь",
+        "letter": "-"
+    },
+    "31": {
+        "character": "Э",
+        "letter": [
+            "e",
+            "è"
+        ]
+    },
+    "32": {
+        "character": "Ю",
+        "letter": [
+            "yu",
+            "ju"
+        ]
+    },
+    "33": {
+        "character": "Я",
+        "letter": [
+            "ya",
+            "ja"
+        ]
+    },
+    "34": {
+        "character": "а",
+        "letter": "a"
+    },
+    "35": {
+        "character": "б",
+        "letter": "b"
+    },
+    "36": {
+        "character": "в",
+        "letter": "v"
+    },
+    "37": {
+        "character": "г",
+        "letter": "g"
+    },
+    "38": {
+        "character": "д",
+        "letter": "d"
+    },
+    "39": {
+        "character": "е",
+        "letter": [
+            "ye",
+            "je",
+            "e"
+        ]
+    },
+    "40": {
+        "character": "ё",
+        "letter": [
+            "yo",
+            "jo"
+        ]
+    },
+    "41": {
+        "character": "ж",
+        "letter": "zh"
+    },
+    "42": {
+        "character": "з",
+        "letter": "z"
+    },
+    "43": {
+        "character": "и",
+        "letter": "i"
+    },
+    "44": {
+        "character": "й",
+        "letter": [
+            "y",
+            "i",
+            "j"
+        ]
+    },
+    "45": {
+        "character": "к",
+        "letter": "k"
+    },
+    "46": {
+        "character": "л",
+        "letter": "l"
+    },
+    "47": {
+        "character": "м",
+        "letter": "m"
+    },
+    "48": {
+        "character": "н",
+        "letter": "n"
+    },
+    "49": {
+        "character": "о",
+        "letter": "o"
+    },
+    "50": {
+        "character": "п",
+        "letter": "p"
+    },
+    "51": {
+        "character": "р",
+        "letter": "r"
+    },
+    "52": {
+        "character": "с",
+        "letter": "s"
+    },
+    "53": {
+        "character": "т",
+        "letter": "t"
+    },
+    "54": {
+        "character": "у",
+        "letter": "u"
+    },
+    "55": {
+        "character": "ф",
+        "letter": "f"
+    },
+    "56": {
+        "character": "х",
+        "letter": [
+            "kh",
+            "h"
+        ]
+    },
+    "57": {
+        "character": "ц",
+        "letter": [
+            "ts",
+            "c"
+        ]
+    },
+    "58": {
+        "character": "ч",
+        "letter": "ch"
+    },
+    "59": {
+        "character": "ш",
+        "letter": [
+            "sh",
+            "s"
+        ]
+    },
+    "60": {
+        "character": "щ",
+        "letter": [
+            "shch",
+            "sch"
+        ]
+    },
+    "61": {
+        "character": "ъ",
+        "letter": "-"
+    },
+    "62": {
+        "character": "ы",
+        "letter": "y"
+    },
+    "63": {
+        "character": "ь",
+        "letter": "-"
+    },
+    "64": {
+        "character": "э",
+        "letter": [
+            "e",
+            "è"
+        ]
+    },
+    "65": {
+        "character": "ю",
+        "letter": [
+            "yu",
+            "ju"
+        ]
+    },
+    "66": {
+        "character": "я",
+        "letter": [
+            "ya",
+            "ja"
+        ]
+    }
+}
+
+const russianChartData = [
+    { "subtitle": "Uppercase letters", "content": allRussianCharacterMap }, 
+    //{ "subtitle": "Lowercase letters", "content": russianLowerCaseMap  }, 
+]
